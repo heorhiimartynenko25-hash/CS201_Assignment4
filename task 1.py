d541054de12e6a88e48489863a7bb1d674c3867d
Heorhii Martynenko <heorhii.martynenko.25@kse.org.ua>
2026-04-12 22:31:55 +0300

    statistics printed to console

--- task 1.py
+++ task 1.py
@@ -4,5 +4,5 @@ import numpy as np
 df = pd.read_csv('random_walk.csv')
 #print(df.head())
 
-df["distance"] = (df["x"]^2 + df["y"]^2)
-print(df)
+df["distance"] = (df["x"]**2 + df["y"]**2)
+print(" Максимальна відстань: ", df["distance"].max(), "\n", "Мінімальна відстань: ", df["distance"].min(), "\n", "Середня відстань: ", df["distance"].mean(), "." )
