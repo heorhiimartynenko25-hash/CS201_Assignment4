80fe00c317cc4b8adbf18a0fe341b220e9cac5ab
Heorhii Martynenko <heorhii.martynenko.25@kse.org.ua>
2026-04-12 22:33:43 +0300

    filtering be mean + test

--- task 1.py
+++ task 1.py
@@ -5,4 +5,8 @@ df = pd.read_csv('random_walk.csv')
 #print(df.head())
 
 df["distance"] = (df["x"]**2 + df["y"]**2)
-print(" Максимальна відстань: ", df["distance"].max(), "\n", "Мінімальна відстань: ", df["distance"].min(), "\n", "Середня відстань: ", df["distance"].mean(), "." )
+mean = df["distance"].mean()
+print(" Максимальна відстань: ", df["distance"].max(), "\n", "Мінімальна відстань: ", df["distance"].min(), "\n", "Середня відстань: ", mean, "." )
+
+df_filter = df[df["distance"] > mean]
+print(df_filter)
